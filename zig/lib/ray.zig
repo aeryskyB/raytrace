fee134fdd2afe0878a929ee3377a9ad9ac435a66
aeryskyB <thunderburning987@gmail.com>
2024-08-11 23:31:47 +0530

    shading with surface normal, and few small tweaks

--- zig/lib/ray.zig
+++ zig/lib/ray.zig
@@ -1,43 +1,43 @@
 const std = @import("std");
-const v3 = @import("./vec3.zig");
+const vec3 = @import("./vec3.zig").vec3;
 
 pub const ray = struct {
-    orig: v3.vec3,
-    dir: v3.vec3,
+    orig: vec3,
+    dir: vec3,
 
     pub fn init_() ray {
         return ray{
-            .orig = v3.vec3.init(0, 0, 0),
-            .dir = v3.vec3.init(0, 0, 0),
+            .orig = vec3.init(0, 0, 0),
+            .dir = vec3.init(0, 0, 0),
         };
     }
 
-    pub fn init(orig: v3.vec3, dir: v3.vec3) ray {
+    pub fn init(orig: vec3, dir: vec3) ray {
         return ray{ .orig = orig, .dir = dir };
     }
 
-    pub fn at(orig: v3.vec3, dir: v3.vec3, t: f32) v3.vec3 {
-        return v3.vec3.add(orig, v3.vec3.scale(dir, t));
+    pub fn at(orig: vec3, dir: vec3, t: f32) vec3 {
+        return vec3.add_v(orig, vec3.scale(dir, t));
     }
 };
 
 test "default initialization" {
     const r: ray = ray.init_();
-    const p: v3.vec3 = v3.vec3.init(0, 0, 0);
+    const p: vec3 = vec3.init(0, 0, 0);
     try std.testing.expect(@reduce(.And, r.orig.coord == p.coord) and @reduce(.And, r.dir.coord == p.coord));
 }
 
 test "normal initialization" {
-    const p1: v3.vec3 = v3.vec3.init(1, 2, 3);
-    const p2: v3.vec3 = v3.vec3.init(3, 2, 1);
+    const p1: vec3 = vec3.init(1, 2, 3);
+    const p2: vec3 = vec3.init(3, 2, 1);
     const r: ray = ray.init(p1, p2);
     try std.testing.expect(@reduce(.And, r.orig.coord == p1.coord) and @reduce(.And, r.dir.coord == p2.coord));
 }
 
 test "at" {
-    const orig: v3.vec3 = v3.vec3.init(0, 0, 0);
-    const dir: v3.vec3 = v3.vec3.init(1, 2, 3);
-    const r_at: v3.vec3 = ray.at(orig, dir, 2);
-    const p: v3.vec3 = v3.vec3.init(2, 4, 6);
+    const orig: vec3 = vec3.init(0, 0, 0);
+    const dir: vec3 = vec3.init(1, 2, 3);
+    const r_at: vec3 = ray.at(orig, dir, 2);
+    const p: vec3 = vec3.init(2, 4, 6);
     try std.testing.expect(@reduce(.And, r_at.coord == p.coord));
 }
---
+++ zig/shading_with_surface_normal.zig
@@ -0,0 +1,88 @@
+const std = @import("std");
+const color3 = @import("./lib/color3.zig").color3;
+const vec3 = @import("./lib/vec3.zig").vec3;
+const ray = @import("./lib/ray.zig").ray;
+
+pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
+    const aspect_ratio: f32 = 16.0 / 9.0;
+    const img_w: u10 = 400;
+
+    const img_h: u10 = @as(u10, @intFromFloat(@as(f32, @floatFromInt(img_w)) / aspect_ratio));
+    if (img_h < 1) {
+        img_h = 1;
+    }
+
+    const focal_len: f32 = 1.0;
+
+    // vp -> viewport
+    const vp_h: f32 = 2.0;
+    const vp_w: f32 = vp_h * (@as(f32, @floatFromInt(img_w)) / @as(f32, @floatFromInt(img_h)));
+    const camera_o = vec3.init(0, 0, 0); // o -> origin
+
+    const vp_u: vec3 = vec3.init(vp_w, 0, 0);
+    const vp_v: vec3 = vec3.init(0, -vp_h, 0);
+
+    // pxl -> pixel :)
+    const pxl_delta_u: vec3 = vec3.scale(vp_u, 1 / @as(f32, @floatFromInt(img_w)));
+    const pxl_delta_v: vec3 = vec3.scale(vp_v, 1 / @as(f32, @floatFromInt(img_h)));
+
+    const norm_p: vec3 = vec3.init(0, 0, -focal_len);
+
+    const vp_up_left: vec3 = vec3.add_v(vec3.add_v(camera_o, norm_p), vec3.add_v(vec3.scale(vp_u, -0.5), vec3.scale(vp_v, -0.5)));
+
+    // initial write
+    try stdout.print("P3\n{d} {d}\n255\n", .{ img_w, img_h });
+
+    const pixel_up_left_loc: vec3 = vec3.add_v(vp_up_left, vec3.scale(vec3.add_v(pxl_delta_u, pxl_delta_v), 0.5));
+    var pxl_v: vec3 = undefined;
+    var pxl: vec3 = undefined;
+    var ray_dir: vec3 = undefined;
+    var r: ray = undefined;
+    var clr: color3 = undefined;
+    const sphr_r: f32 = 0.5;
+    for (0..img_h) |j| {
+        pxl_v = vec3.add_v(pixel_up_left_loc, vec3.scale(pxl_delta_v, @as(f32, @floatFromInt(j))));
+        for (0..img_w) |i| {
+            pxl = vec3.add_v(pxl_v, vec3.scale(pxl_delta_u, @as(f32, @floatFromInt(i))));
+            ray_dir = vec3.add_v(pxl, vec3.scale(camera_o, -1));
+            r = ray.init(camera_o, ray_dir);
+            clr = ray_color(r, norm_p, sphr_r);
+            try color3.write_ppm_rgb_pxl(clr);
+        }
+    }
+}
+
+fn hit_sphere(sphr_c: vec3, sphr_r: f32, r: ray) f32 {
+    const co: vec3 = vec3.add_v(sphr_c, vec3.scale(r.orig, -1));
+    const a: f32 = vec3.dot(r.dir, r.dir);
+    const b: f32 = -2 * vec3.dot(r.dir, co);
+    const c: f32 = vec3.dot(co, co) - sphr_r * sphr_r;
+    const discr: f32 = b * b - 4 * a * c;
+    if (discr < 0) {
+        return -1;
+    }
+    return (-b - std.math.sqrt(discr)) / (2 * a);
+}
+
+fn ray_color(r: ray, sphr_c: vec3, sphr_r: f32) color3 {
+    const t: f32 = hit_sphere(sphr_c, sphr_r, r);
+    if (t > 0) {
+        var norm: vec3 = vec3.add_v(ray.at(r.orig, r.dir, t), vec3.scale(sphr_c, -1));
+        norm = vec3.scale(norm, 1 / vec3.length(norm));
+        const f: f32 = 0.5 * 255;
+        var c = color3.init_rgb(f * (norm.coord[0] + 1), f * (norm.coord[1] + 1), f * (norm.coord[2] + 1));
+        color3.clamp_(&c);
+        return c;
+    }
+
+    const v: vec3 = vec3.scale(r.dir, 1 / vec3.length(r.dir));
+    const a: f32 = 0.45 * (v.coord[1] + 1);
+    var c1 = color3.init_rgb(255, 255, 255); // white
+    var c2 = color3.init_rgb(0, 255, 255); // cyan
+    color3.scale_(&c1, 1 - a, 1 - a, 1 - a);
+    color3.clamp_(&c1);
+    color3.scale_(&c2, a, a, a);
+    color3.clamp_(&c2);
+    return color3.mix(c1, c2);
+}
